a63f528af70dc6c1daaf35a24e2e41c94c95d1e3
Harue Lico <44547634+haruelico@users.noreply.github.com>
2019-07-28 22:29:25 +0900

    Support localtunnel.me and serveo.net

--- test/rangrok_test.rb
+++ test/rangrok_test.rb
@@ -8,4 +8,12 @@ class Rangrok::Test < ActiveSupport::TestCase
   test 'allow request from "*.ngrok.io"' do
     assert_includes Rails.application.config.hosts, ".ngrok.io"
   end
+
+  test 'allow request from "*.localtunnel.me"' do
+    assert_includes Rails.application.config.hosts, ".localtunnel.me"
+  end
+
+  test 'allow request from "*.serveo.net"' do
+    assert_includes Rails.application.config.hosts, ".serveo.net"
+  end
 end
